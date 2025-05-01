fc4c2a48465fb690defe314375a614a33d0f4c13
Matheus A. S. Gueff <gueffmatheus@gmail.com>
2025-05-01 18:57:29 -0300

    Correções

--- ContaBancaria/src/main/java/edu/matheus/contabancaria/ContaTerminal.java
+++ ContaBancaria/src/main/java/edu/matheus/contabancaria/ContaTerminal.java
@@ -32,5 +32,6 @@ public class ContaTerminal {
         saldo = sc.nextDouble();
         
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo  + " já está disponível para saque");
+        sc.close();
     }
 }
